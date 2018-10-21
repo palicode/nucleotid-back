a88f10575997a9b1a6167d3fe6b5fa1253662f09
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-10-21 21:34:45 +0200

    Updates db module.

--- src/modules/db.js
+++ src/modules/db.js
@@ -31,25 +31,48 @@ db.connect()
 module.exports.db = db;
 
 /*
+** DATABASE QUERIES
+*/
+
+module.exports.updateModifiedById = async (table, id) => {
+  try {
+    await db.none("UPDATE $1 SET modified = GETDATE() WHERE id=$2",
+		  [table, id]);
+  } catch (err) {
+    throw err;
+  }
+};
+
+/*
 ** DATABASE TABLES
 */
+module.exports.table_password_blacklist = 'password_blacklist';
+module.exports.table_email_token = 'email_token';
 module.exports.table_user = 'user_profile';
-module.exports.table_group = 'group_profile';
-module.exports.table_group_member = 'group_member';
-module.exports.table_group_permissions = 'group_permissions';
+module.exports.table_team = 'team_profile';
+module.exports.table_team_member = 'team_member';
+module.exports.table_team_permissions = 'team_permissions';
 module.exports.table_project = 'project';
 module.exports.table_project_permissions = 'project_permissions';
 module.exports.table_notebook = 'notebook';
-module.exports.table_notebook_step_header = 'notebook_step_header';
-module.exports.table_notebook_step_body = 'notebook_step_body';
+module.exports.table_notebook_step = 'notebook_step';
 
 var tables = [
+  "CREATE TABLE IF NOT EXISTS password_blacklist (\
+       text                varchar(30)  PRIMARY KEY\
+  );",
+  "CREATE TABLE IF NOT EXISTS email_token (\
+       token               varchar(256) PRIMARY KEY,\
+       user_id             bigint       NOT NULL REFERENCES user_profile(id) ON DELETE CASCADE,\
+       validated           boolean      NOT NULL,\
+       created             timestamptz  NOT NULL,\
+       modified            timestamptz\
+  );",
   "CREATE TABLE IF NOT EXISTS user_profile (\
        id    	    	   bigserial	PRIMARY KEY,\
        given_name    	   varchar(50) 	NOT NULL,\
        family_name	   varchar(100) NOT NULL,\
        email		   varchar(100) NOT NULL,\
-       username		   varchar(20),\
        password		   varchar(256),\
        birthdate	   date,\
        photo		   varchar(512),\
@@ -57,31 +80,36 @@ var tables = [
        google_active	   boolean 	NOT NULL,\
        google_id	   varchar(100),\
        google_accesstoken  varchar(100),\
-       created 		   timestamptz 	NOT NULL\
+       created 		   timestamptz 	NOT NULL,\
+       UNIQUE(email)\
    );",
-  "CREATE TABLE IF NOT EXISTS group_profile (\
+  "CREATE TABLE IF NOT EXISTS team_profile (\
        id    	    bigserial    PRIMARY KEY,\
-       group_name   varchar(100) NOT NULL,\
+       team_name    varchar(100) NOT NULL,\
        ownerId	    bigint 	 NOT NULL REFERENCES user_profile(id) ON DELETE SET NULL,\
-       created	    timestamptz  NOT NULL\
+       personal     boolean      NOT NULL,\
+       created	    timestamptz  NOT NULL,\
+       UNIQUE (ownerId, personal) WHERE personal=TRUE\
    );",
-  "CREATE TABLE IF NOT EXISTS group_permissions (\
+  "CREATE UNIQUE INDEX ON team_profile (ownerId, personal) WHERE personal = TRUE;",
+  "CREATE TABLE IF NOT EXISTS team_permissions (\
        userId	    bigint       NOT NULL REFERENCES user_profile(id) ON DELETE CASCADE,\
-       groupId	    bigint 	 NOT NULL REFERENCES group_profile(id) ON DELETE CASCADE,\
+       teamId	    bigint 	 NOT NULL REFERENCES team_profile(id) ON DELETE CASCADE,\
        read         boolean      NOT NULL DEFAULT FALSE,\
        write        boolean      NOT NULL DEFAULT FALSE,\
        create       boolean      NOT NULL DEFAULT FALSE,\
        delete       boolean      NOT NULL DEFAULT FALSE,\
        admin        boolean      NOT NULL DEFAULT FALSE,\
        created	    timtestamptz NOT NULL,\
-       UNIQUE (userId, groupId)\
+       UNIQUE (userId, teamId)\
    );",
   "CREATE TABLE IF NOT EXISTS project (\
        id           bigserial    PRIMARY KEY,\
        title        varchar(250) NOT NULL,\
        description  text         NOT NULL,\
        ownerId      bigint       NOT NULL REFERENCES user_profile(id) ON DELETE SET NULL,\
-       groupId      bigint       NOT NULL REFERENCES group_profile(id) ON DELETE CASCADE,\
+       teamId       bigint       NOT NULL REFERENCES team_profile(id) ON DELETE CASCADE,\
+       public       boolean      NOT NULL DEFAULT TRUE,\
        created      timestamptz  NOT NULL,\
        modified     timestamptz  NOT NULL\
    );",
@@ -90,8 +118,6 @@ var tables = [
        projectId    bigint 	 NOT NULL REFERENCES project(id) ON DELETE CASCADE,\
        read         boolean      NOT NULL DEFAULT FALSE,\
        write        boolean      NOT NULL DEFAULT FALSE,\
-       create       boolean      NOT NULL DEFAULT FALSE,\
-       delete       boolean      NOT NULL DEFAULT FALSE,\
        admin        boolean      NOT NULL DEFAULT FALSE,\
        created	    timtestamptz NOT NULL,\
        UNIQUE (userId, projectId)\
@@ -102,19 +128,20 @@ var tables = [
        description  text,\
        ownerId 	    bigint 	NOT NULL REFERENCES user_profile(id) ON DELETE CASCADE,\
        projectId    bigint      NOT NULL REFERENCES project(id) ON DELETE CASCADE,\
+       public       boolean     NOT NULL DEFAULT TRUE,\
        created 	    timestamptz NOT NULL,\
        modified     timestamptz NOT NULL\
    );",
-  "CREATE TABLE IF NOT EXISTS notebook_step_header (\
-       id    	    bigserial	 PRIMARY KEY,\
+  "CREATE TABLE IF NOT EXISTS notebook_step (\
        notebookId   bigint 	 NOT NULL REFERENCES notebook(id) ON DELETE CASCADE,\
+       order        int          NOT NULL,\
        title 	    varchar(250) NOT NULL,\
        description  text 	 NOT NULL,\
-       format 	    varchar(50)  NOT NULL,\
-   );",
-  "CREATE TABLE IF NOT EXISTS notebook_step_body {\
-       headerId	bigint NOT NULL UNIQUE REFERENCES notebook_step_header(id) ON DELETE CASCADE,\
-       body	text\
+       format 	    varchar(50),\
+       body	    text\
+       created 	    timestamptz NOT NULL,\
+       modified     timestamptz NOT NULL,\
+       PRIMARY KEY (notebookId, order)\
    );"
 ];
 
