04dceb9b505933a52d5cd361868f873ae1601181
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-10-24 18:15:12 +0200

    Updates auth route.

---
+++ src/routes/auth.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const passport = require('passport');
+const oauth = require('../modules/oauth');
+const userController = require('../controllers/user');
+const cors = require('../modules/cors').cors;
+
+// CORS middleware
+router.all('/login', cors({methods: ["POST"]}));
+router.all('/refresh', cors());
+router.all('/logout', cors());
+router.all('/terminate', cors());
+
+// POST Routes.
+router.post('/login', userController.validateCredentials, oauth.newSession);
+
+// GET Routes.
+router.get('/refresh', oauth.extendSession);
+router.get('/logout', oauth.logout);
+router.get('/terminate', oauth.endSessions);
+
+
+// GOOGLE OAUTH2:
+// Implementar manualment la segona part de l'autenticacio amb google OAUTH2.
+// Primer des de React es fara la redireccio a https://accounts.google.com/o/oauth2/v2/auth
+// amb els parametres adequats.
+// Quan google redirigeixi l'usuari ho fara directament a l'API.
+// El server (API) rep de l'usuari el codi generat per google a redirect_uri,
+// El server fa un POST al token endpoint de google amb el codi rebut.
+// Google verifica el codi i envia la informacio de l'usuari logejat.
+
+module.exports = router;
