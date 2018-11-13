bff78d868d00c1e255fc9b5d4e0ecc410c86d0df
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-11-13 17:08:01 +0100

    Adds test cases for 'validateNewUser'

--- test/test.js
+++ test/test.js
@@ -5,6 +5,50 @@ var assert = require('assert');
 // New users test
 new_users = [
   {
+    test: 'missing email',
+    status: 400,
+    error: 'email',
+    user: {
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'missing given_name',
+    status: 400,
+    error: 'given_name',
+    user: {
+      email: 'mike@mike.com',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'missing family_name',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'missing password',
+    status: 400,
+    error: 'password',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
     test: 'email format 1',
     status: 400,
     error: 'email',
@@ -125,7 +169,20 @@ new_users = [
     }
   },
   {
-    test: 'email length',
+    test: 'email empty',
+    status: 400,
+    error: 'email',
+    user: {
+      email: '',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+
+  {
+    test: 'email max length',
     status: 400,
     error: 'email',
     user: {
@@ -148,32 +205,9 @@ new_users = [
       birthdate: '1992-03-21'
     }
   },
+
   {
     test: 'given_name format 2',
-    status: 400,
-    error: 'given_name',
-    user: {
-      email: 'mike@mike.com',
-      given_name: 'M',
-      family_name: 'Springfield',
-      password: 'password',
-      birthdate: '1992-03-21'
-    }
-  },
-  {
-    test: 'given_name format 3',
-    status: 400,
-    error: 'given_name',
-    user: {
-      email: 'mike@mike.com',
-      given_name: 'Michaelangelodasouzabatmanbinsupraman',
-      family_name: 'Springfield',
-      password: 'password',
-      birthdate: '1992-03-21'
-    }
-  },
-  {
-    test: 'given_name format 4',
     status: 400,
     error: 'given_name',
     user: {
@@ -185,7 +219,7 @@ new_users = [
     }
   },
     {
-    test: 'given_name format 5',
+    test: 'given_name format 3',
     status: 400,
     error: 'given_name',
     user: {
@@ -197,7 +231,7 @@ new_users = [
     }
   },
   {
-    test: 'given_name format 6',
+    test: 'given_name format 4',
     status: 400,
     error: 'given_name',
     user: {
@@ -208,7 +242,234 @@ new_users = [
       birthdate: '1992-03-21'
     }
   },
-
+  {
+    test: 'given_name empty',
+    status: 400,
+    error: 'given_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: '',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+    {
+    test: 'given_name min length',
+    status: 400,
+    error: 'given_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'M',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'given_name max length',
+    status: 400,
+    error: 'given_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Michaelangelodasouzabatmanbinsupraman',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name format 1',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield von Bismarck Deux',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name format 2',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield2',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name format 3',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield (Genius)',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name format 4',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield*von Bismarck',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name min length',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'S',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'family_name max length',
+    status: 400,
+    error: 'family_name',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'SpringfieldVeryLongFamilyNameThisIsMoreThanFiftyCharacters',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'password min length',
+    status: 400,
+    error: 'password',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'pass',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'password max length',
+    status: 400,
+    error: 'password',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'ThisIsAVeryLongPasswordThatNoUserWillEverRemember',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'password blacklist',
+    status: 400,
+    error: 'blacklist',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'password',
+      birthdate: '1992-03-21'
+    }
+  },
+  {
+    test: 'birthdate format 1',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '1992-03'
+    }
+  },
+  {
+    test: 'birthdate format 2',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '31-12-1988'
+    }
+  },
+  {
+    test: 'birthdate format 3',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '1988-21-12'
+    }
+  },
+  {
+    test: 'birthdate format 4',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: 'fkfskfdfs'
+    }
+  },
+  {
+    test: 'birthdate format 5',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '1st of May of 1988'
+    }
+  },
+  {
+    test: 'birthdate too old',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '1830-05-24'
+    }
+  },
+  {
+    test: 'birthdate future',
+    status: 400,
+    error: 'birthdate',
+    user: {
+      email: 'mike@mike.com',
+      given_name: 'Mike',
+      family_name: 'Springfield',
+      password: 'thisisagoodpassword',
+      birthdate: '2099-01-12'
+    }
+  },
   {
     test: 'correct format',
     status: 200,
