3e17e97a13d2a4deda36c9e0e6657d0eaaa3a911
Eduard Valera i Zorita <eduardvalera@gmail.com>
2018-11-13 19:16:46 +0100

    Initializes test DB with blacklisted passwords

--- test/test_db.js
+++ test/test_db.js
@@ -1,3 +1,11 @@
 const db = require('../src/modules/db');
+const q  = db.db;
 
-db.wipeDBTables();
+set_db = async function() {
+  await db.wipeDBTables();
+
+  await q.none('INSERT INTO $1~(password) VALUES($2)', [db.tables.password_blacklist, 'password']);
+  await q.none('INSERT INTO $1~(password) VALUES($2)', [db.tables.password_blacklist, '123456']);
+};
+
+set_db();
